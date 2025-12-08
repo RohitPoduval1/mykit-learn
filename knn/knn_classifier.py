31197a4ad7ab68df58430c546e9798ff0ae6862a
Rohit Poduval <rohit.poduval2@gmail.com>
2025-12-07 21:39:48 -0600

    Add verification for classification with wine dataset

--- knn/knn_classifier.py
+++ knn/knn_classifier.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 from knn import KNNBase
 
 
@@ -23,3 +27,30 @@ class KNNClassifier(KNNBase):
         values, counts = np.unique(top_k_labels, return_counts=True)
         majority_vote = values[np.argmax(counts)]
         return majority_vote
+
+def main():
+    X, y = load_wine(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    print("Without Scaling")
+    for k in range(1, 36, 5):
+        knn = KNNClassifier(k, use_weighting=False)
+        knn.fit(X_train, y_train)
+        preds = knn.predict(X_test)
+        accuracy = (preds == y_test).sum() / y_test.shape[0]
+        print(f'\tAccuracy for k={k}: {accuracy:.3f}')
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    print("With Scaling")
+    for k in range(1, 36, 5):
+        knn = KNNClassifier(k, use_weighting=False)
+        knn.fit(X_train, y_train)
+        preds = knn.predict(X_test)
+        accuracy = (preds == y_test).sum() / y_test.shape[0]
+        print(f'\tAccuracy for k={k}: {accuracy:.3f}')
+
+if __name__ == '__main__':
+    main()
